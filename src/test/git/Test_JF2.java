de5a7ba8878ed2a7723fb1d1edaf6226c8794f03
joel fodjo <fodjotj@3il.fr>
2020-05-10 21:05:00 +0200

    creation de la classe Test_JF2

--- src/test/git/Test_JF2.java
+++ src/test/git/Test_JF2.java
@@ -1,10 +1,10 @@
 package test.git;
 
-public class Test_JF {
+public class Test_JF2 {
 	
 	
 	public void bonjour() {
-		System.out.println( "salut" );
+		System.out.println( "Bonjoour" );
 	}
 	
 	
