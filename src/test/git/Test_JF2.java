d5b8ceb4b4667c2b2bb3db980fe1c6534932cb57
ericJoel9 <63714020+ericJoel9@users.noreply.github.com>
2020-05-10 21:48:43 +0200

    Montreuil, Brive

--- src/test/git/Test_JF2.java
+++ src/test/git/Test_JF2.java
@@ -10,9 +10,9 @@ public class Test_JF2 {
 	
 	
 	private String[] adresses = {
-			"14 Rue Mozart, Paris",
+			"14 Rue Mozart, Montreuil",
 			"77 Rue Picasso, Toulouse", 
-			"53 Rue des fleurs, Limoges",
+			"53 Rue des fleurs, Brive",
 	};
 
 	
