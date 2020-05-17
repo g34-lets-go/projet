e5b293bedef7592f6da9d0b6f310abb8d5ec80c5
joel fodjo <fodjotj@3il.fr>
2020-05-17 23:09:08 +0200

    Debut données et liste benevole

--- scripts-sql/projet-postgresql/2-data.sql
+++ scripts-sql/projet-postgresql/2-data.sql
@@ -2,64 +2,64 @@ SET search_path TO projet;
 
 
 -- Supprimer toutes les données
-DELETE FROM compte;
-DELETE FROM velo;
-DELETE FROM type_benevole;
-DELETE FROM benevole;
-DELETE FROM participant;
-DELETE FROM course;
-DELETE FROM type_course;
-DELETE FROM poste;
-DELETE FROM equipe;
-DELETE FROM dossard;
-DELETE FROM etre_affecte;
+--DELETE FROM compte;
+--DELETE FROM velo;
+--DELETE FROM type_benevole;
+--DELETE FROM benevole;
+--DELETE FROM participant;
+--DELETE FROM course;
+--DELETE FROM type_course;
+--DELETE FROM poste;
+--DELETE FROM equipe;
+--DELETE FROM dossard;
+--DELETE FROM etre_affecte;
 
 
 -- Compte
 
-INSERT INTO compte (idcompte, pseudo, motdepasse, email ) VALUES 
-  (1, 'geek', 'geek', 'geek@3il.fr' ),
-  (2, 'chef', 'chef', 'chef@3il.fr' ),
-  (3, 'job', 'job', 'job@3il.fr' );
+--INSERT INTO compte (idcompte, pseudo, motdepasse, email ) VALUES 
+ -- (1, 'geek', 'geek', 'geek@3il.fr' ),
+ -- (2, 'chef', 'chef', 'chef@3il.fr' ),
+ -- (3, 'job', 'job', 'job@3il.fr' );
 
-ALTER TABLE compte ALTER COLUMN idcompte RESTART WITH 4;
+--ALTER TABLE compte ALTER COLUMN idcompte RESTART WITH 4;
 
 
 -- Velo
 
-INSERT INTO velo (id_velo, type_velo, ) VALUES 
-  ( 1, 'VTT' ),
-  ( 2, 'VTT electrique' );
+--INSERT INTO velo (id_velo, type_velo, ) VALUES 
+--  ( 1, 'VTT' ),
+--  ( 2, 'VTT electrique' );
 
-ALTER TABLE velo ALTER COLUMN id_velo RESTART WITH 3;
+--ALTER TABLE velo ALTER COLUMN id_velo RESTART WITH 3;
 
 -- type_benevole
   
-INSERT INTO type_benevole (id_type_benevole  , nom_type_benevole ) VALUES 
-  (1, 'Capitain' ),
-  (2, 'Equipier' ),
-  (3, 'Capitain' ),
-  (4, 'Equipier' );
+--INSERT INTO type_benevole (id_type_benevole  , nom_type_benevole ) VALUES 
+ -- (1, 'Capitain' ),
+  --(2, 'Equipier' ),
+ -- (3, 'Capitain' ),
+ -- (4, 'Equipier' );
 
-ALTER TABLE type_benevole ALTER COLUMN id_type_benevole RESTART WITH 5;
+--ALTER TABLE type_benevole ALTER COLUMN id_type_benevole RESTART WITH 5;
 
 
 -- Benevole
 
-INSERT INTO benevole (matricule_b, nom, prenom, adresse, email, poste_souhaite, permis_conduire, date_naissance, id_type_benevole) VALUES 
-  ( 2020, 'MOUAFO', 'Paul Denilson', '184 avenue albert thomas', 'mouafo@gmail.com', true, 06/06/2020, 1),
-  ( 2021, 'MOUAFO', 'Paul Denilson', '184 avenue albert thomas', 'mouafo@gmail.com', true, 06/06/2020, 2);
+--INSERT INTO benevole (matricule_b, nom, prenom, adresse, email, poste_souhaite, permis_conduire, date_naissance, id_type_benevole) VALUES 
+--  ( 2020, 'MOUAFO', 'Paul Denilson', '184 avenue albert thomas', 'mouafo@gmail.com', true, 06/06/2020, 1),
+--  ( 2021, 'MOUAFO', 'Paul Denilson', '184 avenue albert thomas', 'mouafo@gmail.com', true, 06/06/2020, 2);
 
-ALTER TABLE personne ALTER COLUMN idpersonne RESTART WITH 3;
+--ALTER TABLE benevole ALTER COLUMN matricule_b RESTART WITH 3;
 
 
 -- Participant
 
-INSERT INTO participant (matricule_p, nom, prenom, date_naiss, telephone, sexe, attestation_medical_pieds, attestation_medical_velo, attestation_medical_canoe, frais_paye, repas_supplementaire, id_equipe, id_equipe_etre_coequipier, id_velo ) VALUES 
-  ( 1, 'MOUAFO', 'Paul Denilson' 02/02/2020, '05 55 99 11 11'),
-  ( 2, 'Fax', '05 55 99 11 11' );
+--INSERT INTO participant (matricule_p, nom, prenom, date_naiss, telephone, sexe, attestation_medical_pieds, attestation_medical_velo, attestation_medical_canoe, frais_paye, repas_supplementaire, id_equipe, id_equipe_etre_coequipier, id_velo ) VALUES 
+  --( 1, 'MOUAFO', 'Paul Denilson' 02/02/2020, '05 55 99 11 11'),
+  --( 2, 'Fax', '05 55 99 11 11' );
 
-ALTER TABLE participant ALTER COLUMN participant RESTART WITH 3;
+--ALTER TABLE participant ALTER COLUMN participant RESTART WITH 3;
 
 
 -- Memo
--- src/projet/view/benevole/ControllerBenevole.java
+++ src/projet/view/benevole/ControllerBenevole.java
@@ -2,28 +2,119 @@ package projet.view.benevole;
 
 import javax.inject.Inject;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import jfox.javafx.util.UtilFX;
 import jfox.javafx.view.IManagerGui;
+import projet.data.Benevole;
+import projet.data.Service;
 import projet.view.EnumView;
+import projet.view.service.ModelService;
 
 public class ControllerBenevole {
+	// Composants de la vue
+	    
+	    @FXML
+		private ListView<Benevole>	listView;
+		@FXML
+		private Button				buttonEditer;
+//		@FXML
+//		private Button				buttonSupprimer;
+		// Autres champs
+		
+		@Inject
+		private IManagerGui			managerGui;
+		@Inject
+		private ModelBenevole		modelBenevole;
+		
+//		
+		// Initialisation du Controller
 
-	@Inject
-	private IManagerGui			managerGui;
+		@FXML
+		private void initialize() {
+
+			// Data binding
+			listView.setItems( modelBenevole.getListe() );
+			
+			listView.setCellFactory(  UtilFX.cellFactory( item -> item.getNomBene() ));
+			
+			// Configuraiton des boutons
+			listView.getSelectionModel().selectedItemProperty().addListener(
+					(obs, oldVal, newVal) -> {
+						configurerBoutons();
+			});
+			configurerBoutons();
+
+		}
+		
+		public void refresh() {
+			modelBenevole.actualiserListe();
+			UtilFX.selectInListView( listView, modelBenevole.getCourant() );
+			listView.requestFocus();
+		}
+
+		
+    //Actions
 	
-	@FXML public void doAccueil() {
+	@FXML 
+	public void doAccueil() {
 		managerGui.showView( EnumView.Accueil);
 	}
-	
-	@FXML public void doParticipant() {
-		managerGui.showView( EnumView.Participant);
+	@FXML 
+	public void doParticipant() {
+	managerGui.showView( EnumView.Participant);
 	}
 	
 	@FXML public void doBenevole() {
 		managerGui.showView( EnumView.Benevole);
 	}
 	
-	@FXML public void doBenevoleAjouter() {
-		managerGui.showView( EnumView.BenevoleAjouter);
+	@FXML
+	public void doBenevoleAjouter() {
+		modelBenevole.preparerAjouter();;
+		managerGui.showView( EnumView.BenevoleAjouter );
 	}
+/*
+	@FXML
+	public void doBenevoleEditer() {
+		Service item = listView.getSelectionModel().getSelectedItem();
+		if ( item == null ) {
+			managerGui.showDialogError( "Aucun élément n'est sélectionné dans la liste.");
+		} else {
+			modelBenevole.preparerModifier(item);
+			managerGui.showView( EnumView.BenevoleAjouter );
+		}
+	}
+*/
+	// Gestion des évènements
+
+	// Clic sur la liste
+	@FXML
+	private void gererClicSurListe( MouseEvent event ) {
+		if (event.getButton().equals(MouseButton.PRIMARY)) {
+			if (event.getClickCount() == 2) {
+				if ( listView.getSelectionModel().getSelectedIndex() == -1 ) {
+					managerGui.showDialogError( "Aucun élément n'est sélectionné dans la liste.");
+				} else {
+//					doBenevoleEditer();
+				}
+			}
+		}
+	}
+	// Méthodes auxiliaires
+	
+		private void configurerBoutons() {
+			
+	    	if( listView.getSelectionModel().getSelectedItems().isEmpty() ) {
+				buttonEditer.setDisable(true);
+				//buttonSupprimer.setDisable(true);
+			} else {
+				buttonEditer.setDisable(false);
+				//buttonSupprimer.setDisable(false);
+			}
+		}
 }
