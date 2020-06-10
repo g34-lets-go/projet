0c24ffd2dc69197c886d2b889ad1cbee1812585e
Wilfried SINKAM <sinkamwilfried@gmail.com>
2020-06-10 19:36:57 +0200

    Mise à jour Sauvons Paul II

--- src/projet/view/participant/ControllerParticipantAjouter.java
+++ src/projet/view/participant/ControllerParticipantAjouter.java
@@ -21,10 +21,10 @@ import projet.view.EnumView;
 import projet.view.equipe.ModelEquipe;
 
 public class ControllerParticipantAjouter {
-	
+
 	@FXML private TextField 		idEquipe;
 	@FXML private TextField 		nomEquipe;
-	
+
 	@FXML private TextField		nomC;	
 	@FXML private TextField		prenomC;	
 	@FXML private DatePicker	dateNC;	
@@ -32,7 +32,7 @@ public class ControllerParticipantAjouter {
 	@FXML private TextField		adresseC;
 	@FXML private TextField		emailC;	
 	@FXML private CheckBox		attestationC;
-	
+
 	@FXML private TextField		nomE;	
 	@FXML private TextField		prenomE;	
 	@FXML private DatePicker	dateNE;	
@@ -44,158 +44,155 @@ public class ControllerParticipantAjouter {
 	@FXML private ToggleGroup	toggleGroupChoixBol;
 	@FXML private RadioButton	choixMiniBol;
 	@FXML private RadioButton	choixGrandBol;
-	
+
 	@FXML private ToggleGroup	toggleGroupCategorie;
 	//@FXML private RadioButton	
-	
+
 	@FXML private TextField		repasSupp;
 	@FXML private TextField		prixrepasSupp;
 
 	@Inject	private IManagerGui			managerGui;
 	@Inject private ModelParticipant modelParticipant;
 	@Inject private ModelEquipe modelEquipe;
-	
+
 	private Equipe 		equipe ;
-	
-	
+
+
 	public void initialize() {
 
 		equipe = modelEquipe.getCourant();
 
 		Participant courantCapitain = equipe.getCapitaine();
 		Participant courantEquipier = equipe.getEquipier();
-		
+
 		//Participant courantCapitain = modelParticipant.getCourantCapitain();
 		//Participant courantEquipier = modelParticipant.getCourantEquipier();
-		
+
 		idEquipe.textProperty().bindBidirectional(equipe.idProperty(), new ConverterStringInteger());
 		nomEquipe.textProperty().bindBidirectional(equipe.nomProperty());
-		
+
 		// Capitain
 		nomC.textProperty().bindBidirectional( courantCapitain.nomProperty() );
 		prenomC.textProperty().bindBidirectional( courantCapitain.prenomProperty() );
 
 		UtilFX.bindBidirectional( dateNC.getEditor(), courantCapitain.dateNProperty(), new ConverterStringLocalDate() );
-		
+
 		telC.textProperty().bindBidirectional( courantCapitain.telProperty(), new ConverterStringInteger() );
 		adresseC.textProperty().bindBidirectional( courantCapitain.adresseProperty() );
 		emailC.textProperty().bindBidirectional( courantCapitain.emailProperty() );
-		
+
 		attestationC.selectedProperty().bindBidirectional( courantCapitain.attestationProperty() );
-		
+
 		// Equipier
 		nomE.textProperty().bindBidirectional( courantEquipier.nomProperty() );
 		prenomE.textProperty().bindBidirectional( courantEquipier.prenomProperty() );
 
 		UtilFX.bindBidirectional( dateNE.getEditor(), courantEquipier.dateNProperty(), new ConverterStringLocalDate() );
-		
+
 		telE.textProperty().bindBidirectional( courantEquipier.telProperty(), new ConverterStringInteger() );
 		adresseE.textProperty().bindBidirectional( courantEquipier.adresseProperty() );
 		emailE.textProperty().bindBidirectional( courantEquipier.emailProperty() );
-		
+
 		attestationE.selectedProperty().bindBidirectional( courantEquipier.attestationProperty() );
-		
+
 		/* Repas supplémentaire*/
-		
+
 		StringConverter<String> monConverteur = new StringConverter<String>() {
 
-            @Override
-            public String fromString(String repas ) {
-                try {
-                    int d = Integer.parseInt(repas);
-                    d=d*7;
-                    return String.format("%d €", d);                    
-                } catch (Exception e) {
-                    return ("");
-                }
-            }
+			@Override
+			public String fromString(String repas ) {
+				try {
+					int d = Integer.parseInt(repas);
+					d=d*7;
+					return String.format("%d €", d);                    
+				} catch (Exception e) {
+					return ("");
+				}
+			}
 
- 
 
-            @Override
-            public String toString(String prixrepas) {
-                try {
-                    int d = Integer.parseInt(prixrepas) ;
-                    d=d/7;
-                    return String.format("%d", d);
-                } catch (Exception e) {
-                    return ("");
-                }
-            }
-        };
-        Bindings.bindBidirectional(repasSupp.textProperty(),prixrepasSupp.textProperty(),monConverteur);
-		
+
+			@Override
+			public String toString(String prixrepas) {
+				try {
+					int d = Integer.parseInt(prixrepas) ;
+					d=d/7;
+					return String.format("%d", d);
+				} catch (Exception e) {
+					return ("");
+				}
+			}
+		};
+		Bindings.bindBidirectional(repasSupp.textProperty(),prixrepasSupp.textProperty(),monConverteur);
+
 		// Choix du bol
 		toggleGroupChoixBol = new ToggleGroup();
 		//choixMiniBol.setToggleGroup(toggleGroupChoixBol);
 		//choixGrandBol.setToggleGroup(toggleGroupChoixBol);
-		
+
 		/*toggleGroupChoixBol.selectedToggleProperty().addListener( obs -> actualiserStatutDansModele() ) ;
 		courantCapitain.frais_payeProperty().addListener(  obs -> actualiserChoixDansVue() );
 		courantEquipier.frais_payeProperty().addListener(  obs -> actualiserChoixDansVue() );
 		actualiserChoixDansVue();*/
-		
+
 
 		toggleGroupChoixBol.selectedToggleProperty().addListener( obs -> actualiserStatutDansModele() ) ; 
 		courantCapitain.frais_payeProperty().addListener(  obs -> actualiserChoixDansVue() );
 		courantEquipier.frais_payeProperty().addListener(  obs -> actualiserChoixDansVue() );
 		//actualiserChoixDansVue();
-		
-<<<<<<< HEAD
+
 		toggleGroupCategorie = new ToggleGroup();
 		toggleGroupCategorie.selectedToggleProperty().addListener( obs -> actualiserCategorieDansModel() );
-=======
-		prixrepasSupp.setText("0 €");
->>>>>>> branch 'master' of https://github.com/g34-lets-go/projet.git
 
-		
+		prixrepasSupp.setText("0 €");
+
+
 	}
-	
+
 	@FXML public void doAccueil() {
 		managerGui.showView( EnumView.Accueil);
 	}
-	
+
 	@FXML public void doParticipant() {
 		managerGui.showView( EnumView.Participant);
 	}
-	
+
 	@FXML 
 	public void doBenevole() {
 		managerGui.showView( EnumView.Benevole);
 	}
-	
+
 	@FXML public void doParticipant_Ajouter() {
 		modelParticipant.preparerAjouter();
 		managerGui.showView( EnumView.ParticipantAjouter);
 	}
-	
+
 	@FXML public void doEnregistrer() {
 		modelEquipe.validerMiseAJour();
-		
+
 		modelEquipe.preparerAjouter();
 		managerGui.showDialogMessage("Vous venez d'ajouter une nouvelle équipe !");
 		managerGui.showView( EnumView.Participant);
-		
+
 	}
-	
+
 	@FXML public void doClub() {		
 		managerGui.showView( EnumView.ParticipantEquipe);
 	}
 
-	
+
 	@FXML public void doAnnuler() {
 		managerGui.showView( EnumView.ParticipantAjouter);
-		
+
 	}
-	
+
 	@FXML public void docalculer() {
 		System.out.println("Calul prix");
-		Participant val, prix;
-		
+
 	}
-		
-	
-	
+
+
+
 	private void actualiserStatutDansModele() {
 		// Modifie le statut en fonction du bouton radio sélectionné 
 		Toggle bouton = toggleGroupChoixBol.getSelectedToggle();
@@ -206,7 +203,7 @@ public class ControllerParticipantAjouter {
 		//modelEquipe.getCourant().getEquipier().setFrais_paye(choix);
 		modelEquipe.getCourant().setIdCourse(choix);
 	}
-	
+
 	private void actualiserChoixDansVue() {
 		// Sélectionne le bouton radio correspondant au statut
 		int choixC = modelParticipant.getCourantCapitain().getFrais_paye();
@@ -215,7 +212,7 @@ public class ControllerParticipantAjouter {
 		Toggle boutonE = toggleGroupChoixBol.getToggles().get( choixE );
 		toggleGroupChoixBol.selectToggle(  boutonC );
 		toggleGroupChoixBol.selectToggle(  boutonE );
-		
+
 		/*int choixC = modelEquipe.getCourant().getCapitaine().getFrais_paye();
 		int choixE = modelEquipe.getCourant().getEquipier().getFrais_paye();
 		Toggle boutonC = toggleGroupChoixBol.getToggles().get( choixC );
@@ -223,18 +220,18 @@ public class ControllerParticipantAjouter {
 		toggleGroupChoixBol.selectToggle(  boutonC );
 		toggleGroupChoixBol.selectToggle(  boutonE );*/
 	}
-	
+
 	private void actualiserCategorieDansModel() {
 		Toggle bouton = toggleGroupCategorie.getSelectedToggle();
 		int choix = toggleGroupCategorie.getToggles().indexOf( bouton  );
 		System.out.println("---------------------"+choix);
-		
+
 		modelParticipant.getCourantCapitain().setFrais_paye( choix );
 		modelParticipant.getCourantEquipier().setFrais_paye( choix );
 		//modelEquipe.getCourant().getCapitaine().setFrais_paye(choix);
 		//modelEquipe.getCourant().getEquipier().setFrais_paye(choix);
 		modelEquipe.getCourant().setIdCourse(choix);
 	}
-	
-	
+
+
 }
